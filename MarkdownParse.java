49907820e7e87e9dbef886a6ca676f7aa98c49a9
njmorales <njmorales@ucsd.edu>
2022-01-19 14:50:01 -0800

    found infinite loop

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,11 +10,14 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
+        System.out.println("Markdown length: " + markdown.length());
         while(currentIndex < markdown.length()) {
+            System.out.println("currentINdex: " + currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            System.out.println("closeParen:  " + closeParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
